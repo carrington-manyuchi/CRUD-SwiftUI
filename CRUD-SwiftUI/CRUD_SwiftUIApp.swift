6d9434da414cc6fa34a272baa81c49fcb8b9cdff
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-04-01 18:29:48 +0200

    Initial Commit

---
+++ CRUD-SwiftUI/CRUD_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUD_SwiftUIApp.swift
+//  CRUD-SwiftUI
+//
+//  Created by Manyuchi, Carrington C on 2025/04/01.
+//
+
+import SwiftUI
+
+@main
+struct CRUD_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
